3301c8ec3567cc099f9a943c6e7eb232dfe39935
Rabii <rabi3jouira@gmail.com>
2021-11-07 23:44:00 +0100

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -113,4 +113,3 @@ print(short_data['Step2_SentimentText'])
 print('---------Capitalization---------')
 short_data['Step6_SentimentText'] = short_data['Step2_SentimentText'].apply( lambda x : ' '.join( [ word.upper() for word in x.split() ]))
 print(short_data['Step6_SentimentText'])
-
